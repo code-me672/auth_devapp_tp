035389f7ceb39cfb7571c153ef0e8ffd97374cf2
mariamnacini-ai <mariamnacini@gmail.com>
2026-05-28 13:44:54 +0000

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,18 @@
+# Importation des outils SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Adresse de la base de données SQLite
 DATABASE_URL = "sqlite:///./secure_auth.db"
 
+# Création du moteur de connexion à la base de données
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
+# Création d'une session pour communiquer avec la base
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
@@ -17,7 +21,7 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-
+# Fonction permettant d'obtenir une connexion à la base
 def get_db():
     db = SessionLocal()
     try:
