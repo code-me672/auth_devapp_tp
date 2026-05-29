077f674c245ce9a3e243038585600150996bcc08
mariamnacini-ai <mariamnacini@gmail.com>
2026-05-29 10:53:20 +0000

    Update dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,14 +1,19 @@
+# Importation des outils FastAPI
 from fastapi import Depends, HTTPException, Request
+# Importation de l'erreur JWT
 from jose import JWTError
-
+# Importation de la fonction de décodage du token
 from .security import decode_token
 
+# Fonction permettant de récupérer l'utilisateur connecté
 def get_current_user(request: Request):
-
+    
+ # Récupération du token d'accès dans les cookies
     token = request.cookies.get(
         "access_token"
     )
 
+     # Vérifie si le token existe
     if not token:
         raise HTTPException(
             status_code=401,
@@ -16,9 +21,11 @@ def get_current_user(request: Request):
         )
 
     try:
+         # Décodage du token
         payload = decode_token(token)
         return payload
 
+     # Gestion des erreurs de token
     except JWTError:
         raise HTTPException(
             status_code=401,
